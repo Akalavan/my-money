3d1f282dc41f9958a8c837734f3b7aed2b24d6a4
m.vagin <m.vagin@k-telecom.org>
2024-05-08 20:48:15 +0500

    add MonetaryTransactionRestController

---
+++ cash-flow-service/src/main/java/ru/akalavan/budget/controller/payload/UpdateMonetaryTransactionPayload.java
@@ -0,0 +1,33 @@
+package ru.akalavan.budget.controller.payload;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record UpdateMonetaryTransactionPayload(
+        @NotNull(message = "{cash_flow.monetary_transactions.update.errors.name_is_null}")
+        @Size(min = 2, max = 255, message = "{cash_flow.monetary_transactions.update.errors.name_size_is_invalid}")
+        String name,
+
+        @Size(max = 255, message = "{cash_flow.monetary_transactions.update.errors.description_size_is_invalid}")
+        String description,
+
+        @NotNull(message = "{cash_flow.monetary_transactions.update.errors.amount_is_null}")
+        @Digits(integer = 12, fraction = 2)
+        BigDecimal amount,
+
+        @NotNull(message = "{cash_flow.monetary_transactions.update.errors.category_id_is_null}")
+        Integer categoryId,
+
+        @NotNull(message = "{cash_flow.monetary_transactions.update.errors.type_operation_id_is_null}")
+        Integer typeOperationId,
+
+        @NotNull(message = "{cash_flow.monetary_transactions.update.errors.date_operation_id_is_null}")
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+        LocalDateTime dateOperation
+) {
+}
