da02385a232bd473a60f6a9943cbb230d5131938
m.vagin <m.vagin@k-telecom.org>
2024-05-09 21:44:26 +0500

    init my-money-app

---
+++ my-money-app/src/main/java/ru/akalavan/my_money/MyMoneyApplication.java
@@ -0,0 +1,12 @@
+package ru.akalavan.my_money;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMoneyApplication.class, args);
+    }
+}
