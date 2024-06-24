0fdd8acba51a1c7569bd3c8cf54a477f8fce41ce
m.vagin <m.vagin@k-telecom.org>
2024-06-24 18:57:34 +0500

    add FileSystemStorageService

---
+++ cash-flow-service/src/main/java/ru/akalavan/budget/service/StorageFileNotFoundException.java
@@ -0,0 +1,11 @@
+package ru.akalavan.budget.service;
+
+public class StorageFileNotFoundException extends SecurityException {
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
